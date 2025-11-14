03b2e22f5af7330c8bde4495f08c38bfa59e6623
israel <collaisra@gmail.com>
2025-11-14 14:39:23 -0500

    superclase

--- vehiculo.py
+++ vehiculo.py
@@ -1,1 +1,33 @@
-print("hola")
+class Vehiculo:
+    def __init__(self, marca, modelo, anio):
+        self.__marca = marca
+        self.__modelo = modelo
+        self.__anio = anio
+
+    # ----- MARCA -----
+    @property
+    def marca(self):
+        return self.__marca
+
+    @marca.setter
+    def marca(self, valor):
+        self.__marca = valor
+
+    # ----- MODELO -----
+    @property
+    def modelo(self):
+        return self.__modelo
+
+    @modelo.setter
+    def modelo(self, valor):
+        self.__modelo = valor
+
+    # ----- AÑO -------
+    @property
+    def anio(self):
+        return self.__anio
+
+    @anio.setter
+    def anio(self, valor):
+        self.__anio = valor
+
